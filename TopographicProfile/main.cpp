9d8006608e34d64d67ef67fff7dc30432a96052e
Blohh <52373144+Blohh@users.noreply.github.com>
2020-05-03 17:33:19 +0200

    Add main file

---
+++ TopographicProfile/main.cpp
@@ -0,0 +1,15 @@
+#include "Vector.h"
+#include "Matrix.h"
+#include "LineralEquation.h"
+#include <iostream>
+#include <fstream>
+
+int main() {
+	try {
+		
+	}
+	catch (const char* msg) {
+		std::cout << "Exception happened!\n" << msg << std::endl;
+	}
+	return 0;
+}
